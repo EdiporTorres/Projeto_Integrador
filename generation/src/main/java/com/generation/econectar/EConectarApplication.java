bcfbe97706d9b4ebd8c89cbecf41172f57a1ad06
eduardowada <156962925+eduardowada@users.noreply.github.com>
2024-07-05 14:01:12 -0300

    Finalizado task 03

--- generation/src/main/java/com/generation/econectar/EConectarApplication.java
+++ generation/src/main/java/com/generation/econectar/EConectarApplication.java
@@ -1,4 +1,4 @@
-package com.generation.generation;
+package com.generation.econectar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ generation/src/main/java/com/generation/econectar/model/CategoriaModel.java
@@ -0,0 +1,51 @@
+package com.generation.econectar.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class CategoriaModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome da categoria é obrigatório!")
+	@Size(min = 1, max = 255, message = "O nome da categoria deve conter no mínimo 1 e no máximo 255 caracteres")
+	private String nome_categoria;
+	
+	@NotBlank(message = "A descrição é obrigatória!")
+	@Size(min = 1, max = 1000, message = "A descrição deve conter no mínimo 1 e no máximo 1000 caracteres")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome_categoria() {
+		return nome_categoria;
+	}
+
+	public void setNome_categoria(String nome_categoria) {
+		this.nome_categoria = nome_categoria;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
