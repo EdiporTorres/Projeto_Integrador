3a8a2e128cd934e6b74399b9deacee09bdeb4f34
Édipo Reis <ediporeiss@gmail.com>
2024-07-05 13:20:01 -0300

    Commit inicial

---
+++ generation/src/main/java/com/generation/generation/EConectarApplication.java
@@ -0,0 +1,13 @@
+package com.generation.generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EConectarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EConectarApplication.class, args);
+	}
+
+}
---
+++ generation/src/test/java/com/generation/generation/EConectarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EConectarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
