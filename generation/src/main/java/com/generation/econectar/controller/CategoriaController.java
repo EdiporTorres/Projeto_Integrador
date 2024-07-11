12b5943c5fe95ca705324caae6493d615935641e
Édipo Reis <ediporeiss@gmail.com>
2024-07-11 09:42:18 -0300

    Update Task4

--- generation/src/main/java/com/generation/econectar/controller/CategoriaController.java
+++ generation/src/main/java/com/generation/econectar/controller/CategoriaController.java
@@ -1,11 +1,15 @@
 package com.generation.econectar.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.econectar.model.CategoriaModel;
 import com.generation.econectar.repository.CategoriaRepository;
 
 @RestController
